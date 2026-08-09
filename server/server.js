2c0c0141c15ee0b72b3af73c174cfaf55ab5931a
saravana2413 <saravanaprasanth2000@gmail.com>
2026-08-09 22:26:21 +0530

    Prepare application for deployment

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ mongoose
   .then(() => {
     console.log("MongoDB connected successfully");
 
-    app.listen(PORT, () => {
+    app.listen(PORT, "0.0.0.0", () => {
       console.log(`Server running on http://localhost:${PORT}`);
     });
   })
